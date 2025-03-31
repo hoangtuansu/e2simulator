a32af2b00b544d9d7103f0ea5ed465c6e3d6c6ef
Nabe, Mory Moussa <mory-moussa.nabe.1@ens.etsmtl.ca>
2025-03-31 11:06:27 +0000

    Update file encode_kpm_indication.cpp

--- kpm_e2sm/src/kpm/encode_kpm_indication.cpp
+++ kpm_e2sm/src/kpm/encode_kpm_indication.cpp
@@ -18,55 +18,55 @@
 #include "encode_kpm_indication.hpp"
 #include "E2SM-KPM-IndicationMessage.h"
 #include "E2SM-KPM-IndicationMessage-Format1.h"
-#include "MeasurementInfoItem.h"
-#include "MeasurementRecordItem.h"
+#include "E2SM-KPM-IndicationHeader.h"
+#include "E2SM-KPM-IndicationHeader-Format1.h"
 #include "MeasurementData.h"
-#include "MeasurementDataItem.h"
+#include "MeasurementRecordItem.h"
+#include "MeasurementInfoList.h"
+#include "MeasurementInfoItem.h"
+#include "E2SM-KPM-EventTriggerDefinition.h"
+#include "E2SM-KPM-EventTriggerDefinition-Format1.h"
+#include "RIC-EventTriggerStyle-Type.h"
 #include "asn_application.h"
 #include "asn_internal.h"
+#include "E2AP-PDU.h"
+#include "RICindication.h"
 #include <vector>
 #include <iostream>
-#include <cstdlib>  // For calloc
+#include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
-bool encode_kpm_indication(const string& measurement_name, double value1, int64_t value2, vector<unsigned char>& buffer) {
-    // Allocation du message d'indication principal
+bool encode_kpm_indication(const string& kpmType, double value1, int64_t value2, std::vector<unsigned char>& buffer) {
     E2SM_KPM_IndicationMessage_t* ind_msg = (E2SM_KPM_IndicationMessage_t*)calloc(1, sizeof(E2SM_KPM_IndicationMessage_t));
-    if (!ind_msg) return false;
-
     ind_msg->indicationMessage_formats.present = E2SM_KPM_IndicationMessage__indicationMessage_formats_PR_indicationMessage_Format1;
 
     E2SM_KPM_IndicationMessage_Format1_t* format1 = (E2SM_KPM_IndicationMessage_Format1_t*)calloc(1, sizeof(E2SM_KPM_IndicationMessage_Format1_t));
-    if (!format1) return false;
 
-    // Données de mesure
+    MeasurementInfoItem_t* measInfoItem = (MeasurementInfoItem_t*)calloc(1, sizeof(MeasurementInfoItem_t));
+    OCTET_STRING_fromString(&measInfoItem->measType.choice.measName, kpmType.c_str());
+    measInfoItem->measType.present = MeasurementType_PR_measName;
+    ASN_SEQUENCE_ADD(&format1->measInfoList.list, measInfoItem);
+
     MeasurementDataItem_t* measItem = (MeasurementDataItem_t*)calloc(1, sizeof(MeasurementDataItem_t));
-    if (!measItem) return false;
 
-    // Mesure 1 (valeur float)
     MeasurementRecordItem_t* mri1 = (MeasurementRecordItem_t*)calloc(1, sizeof(MeasurementRecordItem_t));
     mri1->present = MeasurementRecordItem_PR_real;
     mri1->choice.real = value1;
+    ASN_SEQUENCE_ADD(&measItem->measRecord.list, mri1);
 
-    // Mesure 2 (valeur entiere)
     MeasurementRecordItem_t* mri2 = (MeasurementRecordItem_t*)calloc(1, sizeof(MeasurementRecordItem_t));
     mri2->present = MeasurementRecordItem_PR_integer;
-    ASN_STRUCT_RESET(asn_DEF_INTEGER, &mri2->choice.integer);
-    if (asn_long2INTEGER(&mri2->choice.integer, value2) != 0) return false;
-
-    // Ajout dans le record
-    ASN_SEQUENCE_ADD(&measItem->measRecord.list, mri1);
+    if (asn_long2INTEGER(&(mri2->choice.integer), value2) != 0) return false;
     ASN_SEQUENCE_ADD(&measItem->measRecord.list, mri2);
 
-    // Ajout dans MeasurementData
     ASN_SEQUENCE_ADD(&format1->measData.list, measItem);
 
-    // Assignation finale
-    ind_msg->indicationMessage_formats.choice.indicationMessage_Format1 = *format1;
+    ind_msg->indicationMessage_formats.choice.indicationMessage_Format1 = format1;
 
-    // Encodage ASN.1
-    asn_enc_rval_t ec = der_encode_to_buffer(&asn_DEF_E2SM_KPM_IndicationMessage, ind_msg, buffer.data(), buffer.size());
-
-    return ec.encoded > 0;
+    // Encodage
+    asn_enc_rval_t enc_ret = aper_encode_to_vector(&asn_DEF_E2SM_KPM_IndicationMessage, NULL, ind_msg, buffer);
+    ASN_STRUCT_FREE(asn_DEF_E2SM_KPM_IndicationMessage, ind_msg);
+    return (enc_ret.encoded != -1);
 }
