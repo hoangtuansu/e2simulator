e6ab67e9156b5bb78dcda59d6d035e48f61e5641
Firmin <firmin.nanfack-kemda.1@ens.etsmtl.ca>
2025-06-21 20:59:25 +0000

    Ajout du support RC (rc_e2sm), correction handler E2AP, mise à jour Dockerfile

--- rc_e2sm/rc_subscription_handler.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-// Fonction callback pour traiter un message E2SM-RC
-void ran_control_callback(void* data);
-
-// Fonction pour enregistrer le callback
-void register_rc_subscription_callback(int action_id, void (*callback)(void*));
