1d5daeb911abec022efe2c70148d52eb1df8e3b9
Tsombou Kana, Jordan <jordan.tsombou-kana.1@ens.etsmtl.ca>
2025-07-03 21:16:10 +0000

    Update file generate_data.py

--- ad/generate_data.py
+++ ad/generate_data.py
@@ -22,18 +22,14 @@ ue_types = ['Voice', 'MBB']
 # Initialisation des données simulées
 np.random.seed(42)
 data = {
-    'allocation_policy': np.random.choice(allocation_policies, num_samples),
-    'ue_type': np.random.choice(ue_types, num_samples),
-    'user_outage_percent': np.random.uniform(2.5, 10.0, num_samples),  # en %
+   
     'throughput_dl_mbps': np.random.uniform(0.1, 3000, num_samples),  # DL
     'throughput_ul_mbps': np.random.uniform(0.1, 1000, num_samples),  # UL
     'radio_resource_utilization_percent': np.random.uniform(10, 100, num_samples),
     'available_prbs_dl': np.random.choice([25, 50], num_samples),  # 5 MHz = 25 PRBs ; 10 MHz = 50 PRBs
     'available_prbs_ul': np.random.choice([25, 50], num_samples),
     'num_active_ues': np.random.randint(10, 100, num_samples),
-    'ml_classification_accuracy': np.random.uniform(80, 95, num_samples),  # en %
-    'ml_training_time_sec': np.random.uniform(5, 120, num_samples)
-}
+    
 
 # Création du DataFrame
 metric_data_df = pd.DataFrame(data)
